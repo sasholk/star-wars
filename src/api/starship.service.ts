ae8f3f795c330140b1606704d42e0fe960270155
Oleksandr Boiko <iccc97@gmail.com>
2024-10-29 16:27:08 +0200

    add little fix with type

--- src/api/starship.service.ts
+++ src/api/starship.service.ts
@@ -1,6 +1,11 @@
 import BaseService from './base.service'
+import { Starship } from '@/types/Ship'
 
 const ENDPOINT = '/starships'
+
+interface StarshipResponse {
+	results: Starship[]
+}
 
 class StarshipService extends BaseService {
 	/**
@@ -9,7 +14,7 @@ class StarshipService extends BaseService {
 	 * @param heroId - The unique identifier of the hero for which to fetch starships.
 	 * @returns A promise that resolves to the data containing the list of starships.
 	 */
-	async getStarshipsForHero(heroId: string) {
+	async getStarshipsForHero(heroId: string): Promise<StarshipResponse> {
 		const query = this.buildQuery({ pilots__contains: heroId })
 		return this.get(`${ENDPOINT}/?${query}`)
 	}
